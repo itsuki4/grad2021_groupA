6ce9a1345ad54bcbc4fe3056b4cdf6c1b500ba8a
Yamamoto029 <ksw2070290@stu.o-hara.ac.jp>
2022-01-27 18:01:15 +0900

    Revert "よろしく"
    This reverts commit cbd9efaa4487b6be92db43c9234a6b949d98c095.

---
+++ src/DummyServlet.java
@@ -0,0 +1,13 @@
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class DummyServlet
+ */
+@WebServlet("/dummy")
+public class DummyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
