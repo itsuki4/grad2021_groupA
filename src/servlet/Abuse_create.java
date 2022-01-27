89fbbf88139d7a810c87613a515867b923c33e51
takagawa <ksw2070089@stu.o-hara.ac.jp>
2022-01-27 14:44:32 +0900

    届

--- src/servlet/Abuse_create.java
+++ src/servlet/Abuse_create.java
@@ -16,6 +16,7 @@ import model.Abuse;
 public class Abuse_create extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/abuse_create.jsp");
 		rd.forward(request, response);
